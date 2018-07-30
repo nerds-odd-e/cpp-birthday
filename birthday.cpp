4d768681f360bd10e1a304040bed56f4767616b4
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2018-07-30 16:50:31 +0800

    add second test and implement the birthday check logic

--- birthday.cpp
+++ birthday.cpp
@@ -1,6 +1,10 @@
+#include <ctime>
+#include <iostream>
 #include "birthday.h"
 
 bool IsBirthday() {
-    return false;
+    time_t now = time(0);
+    auto today = localtime(&now);
+    return today->tm_mon + 1 == 4 && today->tm_mday == 9;
 }
 
--- birthday_unittest.cc
+++ birthday_unittest.cc
@@ -6,5 +6,9 @@ namespace {
     TEST(Birthday, NotABirthday) {
         ASSERT_EQ(false, IsBirthday());
     }
+    
+    TEST(Birthday, IsABirthday) {
+        ASSERT_EQ(true, IsBirthday());
+    }
 
 }
